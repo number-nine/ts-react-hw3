b81aafe8a6d236e9b065761c9cd7fe5cf29c3776
number-nine <sergii.pokhylko@gmail.com>
2023-09-25 17:22:35 +0300

    1-3

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,14 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  // name: string;
+  // department: string;
+  // salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {
+    // this.name = name;
+    // this.department = department;
+    // this.salary = salary;
   }
 
   getEmployeeDetails() {
@@ -33,6 +33,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, salary: number) {
+    super(name, 'sales', salary+1000);
+  }
 }
 
 
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,34 @@
+// import { nanoid } from "../node_modules/nanoid/index"
+const { nanoid } = require("nanoid");
+
+interface IKey<T> {
+  signature: T;
+  getSignature(): T;
+}
+
+type Signature = ReturnType<typeof nanoid>;
+
+class Key implements IKey<Signature> {
+  public signature: Signature;
+
+  constructor() {
+    this.signature = true;
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+
+console.log(nanoid());
+console.log(typeof nanoid());
+
 const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+// const house = new MyHouse(key);
+// const person = new Person(key);
 
-house.openDoor(person.getKey());
+// house.openDoor(person.getKey());
 
-house.comeIn(person);
+// house.comeIn(person);
 
-
-export {};
+// export {};
