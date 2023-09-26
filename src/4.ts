0dd28c1d95ede8a8638f86b64373c9b53455c2b9
number-nine <sergii.pokhylko@gmail.com>
2023-09-26 16:13:54 +0300

    4

--- src/4.ts
+++ src/4.ts
@@ -9,26 +9,53 @@ interface IKey<T> {
 type Signature = ReturnType<typeof nanoid>;
 
 class Key implements IKey<Signature> {
-  public signature: Signature;
+  signature: Signature; // як задається прайват-паблік при використанні інтерфесів? тут не дає, бо в інтерфейсі не задано, в інтерфейсі не дає, бо в типах неможна
 
-  constructor() {
-    this.signature = true;
+  constructor()  {
+    this.signature = true; //nanoid() йому пофіг, тип не перевіряє
   }
-  getSignature() {
+  getSignature(): Signature { //якщо типи задані в інтерфейсі, нашо їх дублювати тут?
     return this.signature;
   }
 }
 
-console.log(nanoid());
-console.log(typeof nanoid());
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+   protected door: boolean;
+  protected lock: Key;
+  protected tenants: Person[] = [];
+    public comeIn(visitor: Person):void {
+        this.tenants.push(visitor);
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(protected lock: Key) {
+    super();
+    this.door = false;
+  }
+  public openDoor(key: Key): void {
+    if (key === this.lock) {
+      this.door = true;
+    }
+  }
+}
+
 
 const key = new Key();
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+const house = new MyHouse(key);
+const person = new Person(key);
 
-// house.openDoor(person.getKey());
+house.openDoor(person.getKey());
 
-// house.comeIn(person);
+house.comeIn(person);
 
 // export {};
